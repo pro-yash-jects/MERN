2804c2b9be342b4fd9487d8020a95a994d1c4e53
pro-yash-jects <yash.yadav231@lpu.in>
2026-01-28 10:15:08 +0530

    commmit

--- regform/script.js
+++ regform/script.js
@@ -1,11 +1,46 @@
-console.log("Script loaded successfully.");
+// console.log("Script loaded successfully.");
 
-const students=[
-    {name:"Alice",age:20,course:"Mathematics"},
-    {name:"Bob",age:22,course:"Physics"},
-    {name:"Charlie",age:19,course:"Computer Science"}
-];
+// const students=[
+//     {name:"Alice",age:20,course:"Mathematics"},
+//     {name:"Bob",age:22,course:"Physics"},
+//     {name:"Charlie",age:19,course:"Computer Science"}
+// ];
+// let a = students.length;
 
-for (let i=0;i<students.length;i++){
-    console.log(`Name: ${students[i].name}, Age: ${students[i].age}, Course: ${students[i].course}`);
+// for (let i=0;i<a;i++){
+//     console.log(`Name: ${students[i].name}, Age: ${students[i].age}, Course: ${students[i].course}`);
+// }
+
+const greet =  function(){
+    console.log("Hello");
 }
+greet();
+
+function calculator(a,b,operation){
+    return operation(a,b);
+}
+
+const add = function(x,y){
+    return x+y;
+}
+
+const subtract = function(x,y){
+    return x-y;
+}
+
+const multiply = function(x,y){
+    return x*y;
+} 
+const divide = function(x,y){
+    if (y !== 0){
+        return x/y;
+    } else {
+        return "Error: Division by zero";
+    }
+}
+
+console.log(calculator(10,5,add));        // Output: 15
+console.log(calculator(10,5,subtract));        
+console.log(calculator(10,5,multiply));     // Output: 50
+console.log(calculator(10,5,divide));      // Output: 2
+console.log(calculator(10,0,divide));      // Output: Error: Division by zero
