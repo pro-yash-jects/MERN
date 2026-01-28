eca54f128aea9d34849075acafecab9c1c21152e
pro-yash-jects <yash.yadav231@lpu.in>
2026-01-28 11:49:49 +0530

    commmit

--- regform/script.js
+++ regform/script.js
@@ -11,36 +11,62 @@
 //     console.log(`Name: ${students[i].name}, Age: ${students[i].age}, Course: ${students[i].course}`);
 // }
 
-const greet =  function(){
-    console.log("Hello");
-}
-greet();
+// const greet =  function(){
+//     console.log("Hello");
+// }
+// greet();
 
-function calculator(a,b,operation){
-    return operation(a,b);
+// function calculator(a,b,operation){
+//     return operation(a,b);
+// }
+
+// const add = function(x,y){
+//     return x+y;
+// }
+
+// const subtract = function(x,y){
+//     return x-y;
+// }
+
+// const multiply = function(x,y){
+//     return x*y;
+// } 
+// const divide = function(x,y){
+//     if (y !== 0){
+//         return x/y;
+//     } else {
+//         return "Error: Division by zero";
+//     }
+// }
+
+// console.log(calculator(10,5,add));        // Output: 15
+// console.log(calculator(10,5,subtract));        
+// console.log(calculator(10,5,multiply));     // Output: 50
+// console.log(calculator(10,5,divide));      // Output: 2
+// console.log(calculator(10,0,divide));      // Output: Error: Division by zero
+
+
+function greet1(){
+    console.log("Hello");    
 }
 
-const add = function(x,y){
-    return x+y;
+const greet2 = function (){
+    console.log("Hello Wolrd");
+}
+function callMe(fn){
+    fn();
 }
 
-const subtract = function(x,y){
-    return x-y;
-}
 
-const multiply = function(x,y){
-    return x*y;
-} 
-const divide = function(x,y){
-    if (y !== 0){
-        return x/y;
-    } else {
-        return "Error: Division by zero";
-    }
-}
+callMe(greet1);
+callMe(greet2);
 
-console.log(calculator(10,5,add));        // Output: 15
-console.log(calculator(10,5,subtract));        
-console.log(calculator(10,5,multiply));     // Output: 50
-console.log(calculator(10,5,divide));      // Output: 2
-console.log(calculator(10,0,divide));      // Output: Error: Division by zero
+
+callMe(function(){
+    console.log("Hello World")
+})
+
+
+const greet3 = (name) => console.log(`Hello ${name}`);
+
+greet3("yash")
