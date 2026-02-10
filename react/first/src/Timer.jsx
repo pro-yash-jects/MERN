4dc3f7835b9826906d0182fc4cc594ae1b946ed6
pro-yash-jects <yash.yadav231@lpu.in>
2026-02-10 10:48:31 +0530

    changess

--- react/first/src/Timer.jsx
+++ react/first/src/Timer.jsx
@@ -1,10 +1,20 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 function Timer(){
     const [count,setCount] = useState(0)
-    setInterval(()=>{
-        setCount(count+1)
-    },2000)
+    // setInterval(()=>{
+    //     setCount(count+1)
+    // },2000)
+    useEffect(()=>{
+        const timer = setInterval(()=>{
+            setCount((prev)=>prev+1)
+        },1000)
+
+        return ()=>{
+            clearInterval(timer)
+        }
+    },[])
+    
     return(
         <>
         <h2>Timer</h2>
